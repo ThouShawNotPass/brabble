2c8f4933e4e0ff0ab7b88899c89d6ff148c86046
Justin Shaw <realjustinshaw@gmail.com>
2020-04-26 13:44:11 -0700

    del

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+require(['nightmare'], function (require) {
+  console.log('Starting!');
+  // const Nightmare = require('nightmare');
+
+  // Nightmare.goto('https://www.cnn.com/')
+  //   .evaluate(() => {
+  //     return document.title;
+  //   })
+  //   .end()
+  //   .then((title) => {
+  //     console.log(title);
+  //   });
+});
